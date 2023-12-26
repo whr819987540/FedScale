1a2bba1bc8ef2426b8e49757f45ab5013f2a7897
whr819987540 <819987540@qq.com>
2023-12-26 17:22:48 +0800

    print info of processes to be killed

--- docker/shutdown.py
+++ docker/shutdown.py
@@ -9,5 +9,9 @@ if job_name == 'all':
 else:
     os.system("ps -ef | grep python | grep 'job_name {}' > fedscale_running_temp".format(job_name))
 
+for l in open("fedscale_running_temp").readlines():
+    print(l.strip())
+    print("kill -9 "+str(l.split()[1]) + " 1>/dev/null 2>&1" + "\n") 
+    
 [os.system("kill -9 "+str(l.split()[1]) + " 1>/dev/null 2>&1") for l in open("fedscale_running_temp").readlines()]
 os.system("rm fedscale_running_temp")
